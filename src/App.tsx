e59c1ce3abdc3e7aa71cca8158f37f7c1480bcd0
Yanio <yanio@Yanio-MacBook-Pro.local>
2021-12-31 09:50:32 -0500

    adding linters config

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Put code here</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
